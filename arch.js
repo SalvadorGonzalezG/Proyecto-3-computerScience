c50940e8411cb1b3143ea38d7c901a29856e00f2
Gonzalez Salvador <salvadorsopas@gmail.com>
2023-05-25 16:48:59 -0600

    3er avance data science

--- arch.js
+++ arch.js
@@ -1,30 +1,44 @@
 
 class Alumno{
-    constructor(nombre, apellidos, edad, materiasInscritas, calificaciones){
+    constructor(nombre, apellidos, edad, tipo){
         this.nombre = nombre;
         this.apellidos = apellidos;
         this.edad = edad;
-        this.materiasInscritas = materiasInscritas;
-        this.calificaciones = calificaciones;
-
-        this.sumaCalif = function sumaCalif(){
-        return `la suma de las calificaciones de ${this.nombre} son:`
+        this.tipo = tipo;
+        
+        /*this.sumaCalif = function sumaCalif(){
+        return `la suma de las calificaciones de ${this.nombre} son:`*/
                                              }
                                                                            }
-            }
+            
+    class Regular extends Alumno{
+        constructor(nombre, apellidos, edad, tipo){
+            super(nombre, apellidos, edad, tipo)
+        }
+    } 
+    class Irregular extends Alumno{
+        constructor(nombre, apellidos, edad, tipo){
+            super(nombre, apellidos, edad, tipo)
+        }
+    }
 
  /*const Raul = new Alumno("Guill","Chavez Hernandez", "16 años", "1", "8, 6, 10")   console.log(Raul.nombre);
     console.log(Raul.apellidos); console.log(Raul.edad) console.log(Raul.materiasInscritas) console.log(Raul.calificaciones) console.log(Raul.sumaCalif())
 */
-    var alumno = JSON.parse(localStorage.getItem('alumno')) || [];
+    var alumno; //JSON.parse(localStorage.getItem('alumno')) || [];
 
-    function crearAlumno () {
+    function crearAlumno() {
         var nombre = document.querySelector("#nombre").value
         var apellidos = document.querySelector("#apellidos").value
         var edad = document.querySelector("#edad").value
+        var tipo = document.querySelector("#tipo").value
 
-        if(edad > 0){
-            alumno = new Alumno(nombre, apellidos, edad)
+        if(tipo == "Regular"){
+            alumno = new Regular(nombre, apellidos, edad, tipo);
+            
+        }
+        if(tipo == "Irregular"){
+            alumno = new Irregular(nombre, apellidos, edad, tipo)
         }
 
         dibujar(alumno)
@@ -33,15 +47,15 @@ class Alumno{
 
     function dibujar(alumno){
         let nodoalumno = document.querySelector("#fist")
-        nodoalumno.innerHTML =
-       // document.querySelector("#alumno").innerHTML +=
+        nodoalumno.innerHTML = 
+       //document.querySelector("#fist").innerHTML +=
     `
     <div>
             <h1> CONFIRMACION DE TUS DATOS: </h1>
-            <p > Tu nombre es: ${alumno.nombre}</p>
+            <p > Tu nombre es: ${alumno.nombre}.</p>
             <p class=""> Tus apellidos son: ${alumno.apellidos}</p>
             <p class=""> Tu edad es: ${alumno.edad} años</p>
-            <h5>NOTA: Da click en confirmar si son correctos tus datos para poder dar de alta las materias que deseas cursar, ¡bienvenido ${alumno.nombre}! ${this.sumaCalif}. </h5>
+            <h5>NOTA: Da click en confirmar si son correctos tus datos para poder dar de alta las materias que deseas cursar, ¡bienvenido ${alumno.nombre}! ${alumno.tipo}. </h5>
             <button id="buton" class="" type="button" onclick ="confirmar()" > Confirmar.</button>
     </div>`;
     }
@@ -51,7 +65,32 @@ class Alumno{
     function confirmar(){
         let nodoconfirmar = document.querySelector("#fist")
         nodoconfirmar.innerHTML =
-    ` gracias por confirmar tus datos
+    ` Gracias por confirmar tus datos ${alumno.nombre} ${alumno.apellidos}, selecciona las MATERIAS que deseas inscribir y los profesores con quienes deseas cursar.
+        <button id ="" onclick="continu()"> CONTINUAR.</button>
     `
     }
     
+    function continu(){
+        let nodocontinue = document.querySelector("#fist")
+        nodocontinue.innerHTML = `
+        ALTA DE MATERIAS <br>
+        <label class="label" for="tipo"> Materias a inscribir. </label>
+        <div >
+            <select id="tipo">
+                <option>Arte.</option>
+                <option>Matematicas.</option>
+                <option>Español</option>
+                <option>Literatura</option>
+            </select>
+            <br>
+        <label> Profesores disponibles:</label>
+            <br>
+            <select>
+            <option> Dr.Antonio Romero Sanchez.</option>
+            <option> Dr.Angel David Pineda.</option>
+            <option> Dra. Maria Salazar L.</option>                    
+            </select> 
+        </div>        
+        `
+
+    }
