2a4039cd0e4c1c7a42b73348ac1a201423c3cb85
Gonzalez Salvador <salvadorsopas@gmail.com>
2023-05-25 02:40:48 -0600

    Segundo avance 3proyecto data science

--- arch.js
+++ arch.js
@@ -13,14 +13,9 @@ class Alumno{
                                                                            }
             }
 
- const Raul = new Alumno("Guill","Chavez Hernandez", "16 años", "1", "8, 6, 10")
-    console.log(Raul.nombre);
-    console.log(Raul.apellidos);
-    console.log(Raul.edad)
-    console.log(Raul.materiasInscritas)
-    console.log(Raul.calificaciones)
-    console.log(Raul.sumaCalif())
-
+ /*const Raul = new Alumno("Guill","Chavez Hernandez", "16 años", "1", "8, 6, 10")   console.log(Raul.nombre);
+    console.log(Raul.apellidos); console.log(Raul.edad) console.log(Raul.materiasInscritas) console.log(Raul.calificaciones) console.log(Raul.sumaCalif())
+*/
     var alumno = JSON.parse(localStorage.getItem('alumno')) || [];
 
     function crearAlumno () {
@@ -35,24 +30,28 @@ class Alumno{
         dibujar(alumno)
         return alumno;
     }
+
     function dibujar(alumno){
-        document.querySelector("#alumno").innerHTML +=
+        let nodoalumno = document.querySelector("#fist")
+        nodoalumno.innerHTML =
+       // document.querySelector("#alumno").innerHTML +=
     `
-    <div class = "card">
+    <div>
             <h1> CONFIRMACION DE TUS DATOS: </h1>
             <p > Tu nombre es: ${alumno.nombre}</p>
             <p class=""> Tus apellidos son: ${alumno.apellidos}</p>
             <p class=""> Tu edad es: ${alumno.edad} años</p>
-            <h5>NOTA: Da click en confirmar si son correctos tus datos para poder dar de alta las materias que deseas cursar bienvenido ${alumno.nombre}. </h5>
-            <button id="button" class="" type="button"> Confirmar.</button>
-
-           
+            <h5>NOTA: Da click en confirmar si son correctos tus datos para poder dar de alta las materias que deseas cursar, ¡bienvenido ${alumno.nombre}! ${this.sumaCalif}. </h5>
+            <button id="buton" class="" type="button" onclick ="confirmar()" > Confirmar.</button>
     </div>`;
-    
-    
     }
-    function clickear(alumno){
-        let clickeando = document.querySelector("#containerSonido")
-        clickeando.innerHTML = `esto es`
-    }
+    
     document.querySelector("#button").addEventListener("click", crearAlumno)
+    
+    function confirmar(){
+        let nodoconfirmar = document.querySelector("#fist")
+        nodoconfirmar.innerHTML =
+    ` gracias por confirmar tus datos
+    `
+    }
+    
